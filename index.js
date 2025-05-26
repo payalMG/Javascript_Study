ad65870b2f1688a680b3e8f0aa7ad0273c6dd9f5
payalMG <gargpayalkhj@gmail.com>
2025-05-26 21:43:03 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log("Hello World");
+
+// console.log("hii");
+
+// console.log(12);
+
+// document.write("Hey")
+
+// keywords- let,const,var.
+
+var num=20;
+var num=85; //reassign the values and re-declare both are executed and this is globally scope.
+console.log(num);
+
+let a = 50;
+a =40; // this is block scope and reassign  the values  but can not re-declare the values.
+console.log(a);
+
+const b=40;
+// b=10; var can not reassign and can not re-declare the values and this is block scope.
+console.log(b);
