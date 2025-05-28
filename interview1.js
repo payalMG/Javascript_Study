3d496510a9a5d02da311864fde4befa692aa5521
payalMG <gargpayalkhj@gmail.com>
2025-05-28 16:35:48 +0900

    Add files via upload

---
+++ interview1.js
@@ -0,0 +1,52 @@
+//Q1- Difference between “ == “ and “ === “ operators.
+// Ans-  “ == “ - Checks if values are the same.
+            //    Ignores the type conversion.
+
+    //   “ === “  - Compares both value and type. 
+                    // First two equals check values and last equal checks datatypes.      
+                    
+//Q2- What are the differences between var, let and const?                    
+//Ans- Var- This is globally scope and it is reassign the values and also re-declaration the variable.
+       //let- This is block scope and it is reassign te values but can not re-declaration the variables.
+       //const- This is block scope and we can not resaasign and re-declaration.
+
+//Q3- What is execution context?    
+//Ans- Execution Context it is Environment where JS runs code.
+    // It tracks what’s happening while the code runs.   
+
+//Q4- What is creation phase and execution phase?    
+//Ans- Creation Phase - Reserves memory, sets undefined, stores functions.
+     //Execution Phase - Runs the code step-by-step.
+
+//Q5- What is hoisting?
+//Ans- Hoisting means JavaScript moves declarations to the top of the code before running it.
+
+//Q6- Difference between undefined vs not defined vs NaN.
+//Ans-  undefined - The variable is declared, but no value is given yet.
+       //not defined - The variable was never declared at all.
+       //NaN - Not a Number (invalid math).
+
+//Q7- How many operators do we have in JS ?    
+//Ans- The main types of operators in JavaScript in a simple way- : 
+
+//1- Arithmetic Operators - +, -, *, /, %, *.
+//2- Comparison Operators - ==, ===, !=, >, <.
+//3- Bitwise Operators.
+//4- Increment and Decrement Operators - (a++,++a,a--,--a,).
+//5 - Logical Operators - (&&, ||,!).
+//6 - Ternary Opeartors - condition ? a : b
+//7 - Comma Operators -(,).
+
+//Q8- Explain Local Scope, Block Scope, Functional Scope and Scope Chain in javascript?
+//Ans-  Local Scope -	Variable inside a function. It Can only be used inside that function.
+//Functional Scope -  var does not follow block scope, only function scope.
+                    //So, var inside a block {} is still visible outside the block (not safe).
+//Block Scope - Variable inside {} using let/const.   
+//Scope Chain - If a variable is not found in the current scope, JavaScript looks up to the outer scope.
+                
+
+
+
+      
+
+     
